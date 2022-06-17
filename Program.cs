7117922df8405b798baf5bbe8292ed46e3e2f09a
Alena <apispallada@gmail.com>
2022-06-18 00:06:58 +0300

    PrintArray added

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string[] StartArray = {"дом", "поле", "сом", "луна", "173", "вишня", "2", "солнце", "сон"};
+
+string PrintArray(string[] array)
+{
+    int size = array.Length;
+    string res = "[ ";
+    for (int i = 0; i < size; i++)
+    {
+        res += $"{array[i]} ";
+    }
+    res += "] ";
+    return res;
+}
