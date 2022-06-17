407e1b769a316618651753fc0baec56b6efda377
Alena <apispallada@gmail.com>
2022-06-18 01:08:55 +0300

    SortArray added

--- Program.cs
+++ Program.cs
@@ -11,3 +11,18 @@ string PrintArray(string[] array)
     res += "] ";
     return res;
 }
+string[] SortArray(string[] array)
+{
+    string[] result = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+            result[i] = array[i];
+    }
+    return result;
+}
+
+System.Console.Write(PrintArray(StartArray));
+string[] resultArray = SortArray(StartArray);
+System.Console.Write(" -> ");
+System.Console.Write(PrintArray(resultArray));
